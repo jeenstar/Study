af737f747aea92fc567eaf2fd6df122d6ba75fb1
jeenstar <jeenstar11@gmail.com>
2016-07-20 17:29:59 +0900

    exercise 4.2.3

--- etc/Exercise/app/src/main/java/com/example/yujeen/exercise/MainActivity.java
+++ etc/Exercise/app/src/main/java/com/example/yujeen/exercise/MainActivity.java
@@ -2,12 +2,34 @@ package com.example.yujeen.exercise;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText edit;
+    private Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        edit = (EditText)findViewById(R.id.edit);
+        btn = (Button)findViewById(R.id.btn);
+
+        btn.setOnClickListener(listener);
     }
+
+    View.OnClickListener listener = new View.OnClickListener(){
+        @Override
+        public void onClick(View v) {
+            String str;
+            if(v.getId() == R.id.btn){
+                str = edit.getText().toString();
+                Toast.makeText(MainActivity.this, str, Toast.LENGTH_SHORT).show();
+            }
+        }
+    };
 }
