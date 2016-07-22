9ca5fa738fd04e4c0646b5e1597fd2ada3570281
jeenstar <jeenstar11@gmail.com>
2016-07-22 17:02:29 +0900

    exercise 8.1

--- etc/Exercise/app/src/main/java/com/example/yujeen/exercise/MainActivity.java
+++ etc/Exercise/app/src/main/java/com/example/yujeen/exercise/MainActivity.java
@@ -1,20 +1,35 @@
 package com.example.yujeen.exercise;
 
+import android.content.Context;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    private RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
+        relativeLayout = (RelativeLayout)findViewById(R.id.relativeLayout1);
+        relativeLayout.setOnTouchListener(TouchListener);
     }
+    class TouchListenerClass implements View.OnTouchListener{
+        @Override
+        public boolean onTouch(View view, MotionEvent motionEvent) {
+            if(motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                Snackbar.make(relativeLayout,"touchtouch",Snackbar.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        }
+    }
+    TouchListenerClass TouchListener = new TouchListenerClass();
 }
